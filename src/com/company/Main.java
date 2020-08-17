28d9cf1f3479eca24f74921f377b4ec9cc198efb
bia-programming <bia.pasca@yahoo.com>
2020-08-17 17:10:10 +0300

    Student class

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Student student=new Student("John",2);
+        System.out.println(student.getName());
+        System.out.println(student.getRoll_no());
+    }
+}
